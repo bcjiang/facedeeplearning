a1af2b33b9c1e204954adb886b88a91c83cf93f2
Baichuan Jiang <baichuan@jhu.edu>
2017-11-04 15:13:19 -0400

    write training part

--- p1a.py
+++ p1a.py
@@ -2,6 +2,7 @@ import torch
 from torch.autograd import Variable
 import torch.nn as nn
 import torch.nn.functional as F
+from torch import optim
 
 class SiameseNet(nn.Module):
 
@@ -31,6 +32,11 @@ class SiameseNet(nn.Module):
             nn.BatchNorm2d(1024),
         )
 
+        self.nn3 = nn.Sequential(
+            nn.Linear(2048, 1)
+            nn.Sigmoid()
+        )
+
     def net_forward(self,x):
         temp = self.nn1(x)
         temp = temp.view(temp.size()[0], -1)
@@ -40,4 +46,35 @@ class SiameseNet(nn.Module):
     def forward(self,x1,x2):
         output1 = self.net_forward(x1)
         output2 = self.net_forward(x2)
-        return output1, output2
+        output12 = torch.cat((output1,output2))
+        output = self.nn3(output12)
+        return output
+
+# Define BCE loss
+class BCEloss(nn.Module):
+    def __init__(self):
+        super(BCEloss, self).__init__()
+
+    def forward(self,output,label):
+        loss = F.binary_cross_entropy_with_logits(output,label)
+        return loss
+
+# Training the net
+net = SiameseNet()
+optimizer = optim.Adam(net.parameters(), lr = 0.0005)
+loss_function = BCEloss()
+total_epoch_num = 100
+for epoch in range(total_epoch_num):
+    for batch_idx, (imgs, labels) in enumerate(train_loader):
+        x = Variable(imgs)
+        y = Variable(labels)
+        optimizer.zero_grad()
+        y_pred = net(img_input1,img_input2)
+        bce_loss = loss_function(y_pred,y)
+        bce_loss.backward()
+        optimizer.step()
+
+        if batch_idx % 100 == 0:
+            print "Epoch %d, Batch %d Loss %f" % (epoch, batch_idx, bce_loss.data[0])
+    
+# Test the net
